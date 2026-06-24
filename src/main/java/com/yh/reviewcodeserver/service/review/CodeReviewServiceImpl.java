60b1e4cf88a67d68ef724aaa8c95ccf5dcb5d737
dydhyun <dydgus625@naver.com>
2026-06-25 03:04:06 +0900

    Test commit

--- src/main/java/com/yh/reviewcodeserver/service/review/CodeReviewServiceImpl.java
+++ src/main/java/com/yh/reviewcodeserver/service/review/CodeReviewServiceImpl.java
@@ -18,8 +18,13 @@ public class CodeReviewServiceImpl implements CodeReviewService{
     @Override
     public void review(ReviewRequest request) {
 
+        log.debug("request={}", request);
+        log.info("diff=[{}]", request.diff());
+        log.info("diff length={}",
+                request.diff() == null ? null : request.diff().length());
+
         if (request.diff() == null || request.diff().isBlank()) {
-            log.info(request.repository() + " repo 커밋 발생. 검토사항 없음.");
+            log.info("{} repo 커밋 발생. 검토사항 없음.", request.repository());
             return;
         }
 
