6e0ff1b02c200918509bdc62949c01f5bb0b5400
dydhyun <dydgus625@naver.com>
2026-06-25 00:00:17 +0900

    refactor: 요청정보 추가

--- src/main/java/com/yh/reviewcodeserver/client/llm/openrouter/OpenRouterClient.java
+++ src/main/java/com/yh/reviewcodeserver/client/llm/openrouter/OpenRouterClient.java
@@ -1,5 +1,6 @@
 package com.yh.reviewcodeserver.client.llm.openrouter;
 
+import com.yh.reviewcodeserver.Dto.ReviewRequest;
 import com.yh.reviewcodeserver.Dto.openrouter.OpenRouterResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
@@ -21,9 +22,78 @@ public class OpenRouterClient {
     private final RestTemplate restTemplate;
     private final OpenRouterProperties openRouterProperties;
 
-    public String review(String diff){
+    public String review(ReviewRequest actionsRequest){
 
-        String prompt = """
+        String commitInfo = getCommitInfo(actionsRequest);
+
+        String prompt = getPrompt(actionsRequest);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(openRouterProperties.getApiKey());
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        Map<String, Object> body = Map.of(
+                "model", "openai/gpt-oss-120b:free",
+                "messages", List.of(
+                        Map.of(
+                                "role", "user",
+                                "content", prompt
+                        )
+                )
+        );
+
+        HttpEntity<Map<String, Object>> springToOpenRouterRequest =
+                new HttpEntity<>(body,headers);
+
+        try {
+            ResponseEntity<OpenRouterResponse> response =
+                    restTemplate.postForEntity(
+                            "https://openrouter.ai/api/v1/chat/completions",
+                            springToOpenRouterRequest,
+                            OpenRouterResponse.class
+                    );
+
+            List<OpenRouterResponse.Choice> openRouterChoices = response.getBody().getChoices();
+            List<Integer> usages = List.of(
+                    response.getBody().getUsage().getPrompt_tokens(),
+                    response.getBody().getUsage().getCompletion_tokens(),
+                    response.getBody().getUsage().getTotal_tokens());
+            System.out.println(usages);
+
+
+            if (openRouterChoices == null || openRouterChoices.isEmpty()) {
+                throw new IllegalStateException("OpenRouter AI 응답이 비어있습니다.");
+            }
+
+            return commitInfo +
+                    "\uD83E\uDD16 AI Review \n" +
+                    openRouterChoices.get(0).getMessage().getContent();
+
+
+        } catch (HttpClientErrorException e) {
+            return e.getResponseBodyAsString();
+        }
+    }
+
+
+
+    private String getCommitInfo(ReviewRequest actionsRequest) {
+
+        String repository = actionsRequest.repository();
+        String authorName = actionsRequest.author();
+        String commitMessage = actionsRequest.commitMessage();
+
+        return """
+                Repository : %s
+                Author : %s
+                Commit : %s
+                """.formatted(repository,authorName,commitMessage);
+    }
+
+
+    private String getPrompt(ReviewRequest actionsRequest){
+
+        return """
                 시니어 백엔드 개발자 관점으로 다음 코드 변경사항을 리뷰해줘.
                 
                 고려할 항목
@@ -41,49 +111,6 @@ public class OpenRouterClient {
                 변경사항:
                 
                 %s
-                """.formatted(diff);
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(openRouterProperties.getApiKey());
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        Map<String, Object> body = Map.of(
-                "model", "openai/gpt-oss-120b:free",
-                "messages", List.of(
-                        Map.of(
-                                "role", "user",
-                                "content", prompt
-                        )
-                )
-        );
-
-        HttpEntity<Map<String, Object>> request =
-                new HttpEntity<>(body,headers);
-
-        try {
-            ResponseEntity<OpenRouterResponse> response =
-                    restTemplate.postForEntity(
-                            "https://openrouter.ai/api/v1/chat/completions",
-                            request,
-                            OpenRouterResponse.class
-                    );
-
-            List<OpenRouterResponse.Choice> openRouterChoices = response.getBody().getChoices();
-            List<Integer> usages = List.of(
-                    response.getBody().getUsage().getPrompt_tokens(),
-                    response.getBody().getUsage().getCompletion_tokens(),
-                    response.getBody().getUsage().getTotal_tokens());
-            System.out.println(usages);
-
-            if (openRouterChoices == null || openRouterChoices.isEmpty()) {
-                throw new IllegalStateException("OpenRouter AI 응답이 비어있습니다.");
-            }
-
-            return "\uD83E\uDD16 AI Review \n"+openRouterChoices.get(0).getMessage().getContent();
-
-        } catch (HttpClientErrorException e) {
-            return e.getResponseBodyAsString();
-        }
+                """.formatted(actionsRequest.diff());
     }
-
 }
