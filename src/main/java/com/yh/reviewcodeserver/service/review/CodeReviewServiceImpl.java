e3ba0842f8dce42b0d1d86277fbd2fe73de7b436
dydhyun <dydgus625@naver.com>
2026-06-25 00:45:11 +0900

    test commit

--- src/main/java/com/yh/reviewcodeserver/service/review/CodeReviewServiceImpl.java
+++ src/main/java/com/yh/reviewcodeserver/service/review/CodeReviewServiceImpl.java
@@ -17,7 +17,6 @@ public class CodeReviewServiceImpl implements CodeReviewService{
     public void review(ReviewRequest request) {
 
         String reviewResult = openRouterClient.review(request);
-        System.out.println(reviewResult);
 
         slackService.sendMessage(reviewResult);
     }
